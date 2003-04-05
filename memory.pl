72495f1ad524047663eb6ae9ba7f270ce8cb07b5
mitch <mitch>
2003-04-05 14:22:20 +0000

    data gathering works

--- memory.pl
+++ memory.pl
@@ -1,5 +1,5 @@
 #!/usr/bin/perl
-# $Id: memory.pl,v 1.1 2003-04-05 14:15:10 mitch Exp $
+# $Id: memory.pl,v 1.2 2003-04-05 14:22:20 mitch Exp $
 #
 # RRD script to display memory usage
 # 2003 (c) by Christian Garbs <mitch@cgarbs.de>
@@ -34,5 +34,25 @@ if ( ! -e $datafile ) {
 		 );
       $ERR=RRDs::error;
       die "ERROR while creating $datafile: $ERR\n" if $ERR;
+      print "created $datafile\n";
 }
 
+# update database
+open PROC, "<", "/proc/meminfo" or die "can't open /proc/meminfo: $!\n";
+my (undef, $mem, $swap) = (<PROC>, <PROC>, <PROC>);
+close PROC or die "can't close /proc/meminfo: $!\n";
+
+chomp $mem;
+my (undef, undef, $used, $free, undef, $buffer, $cache) = split /\s+/, $mem;
+
+chomp $swap;
+my (undef, undef, $swap_used, $swap_free) = split /\s+/, $swap;
+
+print "mem: u$used f$free b$buffer c$cache\n";
+print "swp: u$swap_used f$swap_free\n";
+
+#RRDs::update($datafile,
+#	     time() . ":"
+#	     );
+#$ERR=RRDs::error;
+#die "ERROR while updating $datafile: $ERR\n" if $ERR;
