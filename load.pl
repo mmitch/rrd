d690c2faae851ed7f06e0287024004443ecb1969
mitch <mitch>
2003-04-05 20:53:15 +0000

    include process min/max "corridor"

--- load.pl
+++ load.pl
@@ -1,5 +1,5 @@
 #!/usr/bin/perl
-# $Id: load.pl,v 1.2 2003-04-05 18:41:16 mitch Exp $
+# $Id: load.pl,v 1.3 2003-04-05 20:53:15 mitch Exp $
 #
 # RRD script to display system load
 # 2003 (c) by Christian Garbs <mitch@cgarbs.de>
@@ -38,7 +38,12 @@ if ( ! -e $datafile ) {
 		 "RRA:MAX:0.5:5:600",
 		 "RRA:MAX:0.5:6:700",
 		 "RRA:MAX:0.5:120:775",
-		 "RRA:MAX:0.5:1440:797"
+		 "RRA:MAX:0.5:1440:797",
+		 "RRA:MIN:0.5:1:120",
+		 "RRA:MIN:0.5:5:600",
+		 "RRA:MIN:0.5:6:700",
+		 "RRA:MIN:0.5:120:775",
+		 "RRA:MIN:0.5:1440:797"
 		 );
       $ERR=RRDs::error;
       die "ERROR while creating $datafile: $ERR\n" if $ERR;
@@ -59,13 +64,18 @@ foreach ( [3600, "hour"], [86400, "day"], [604800, "week"], [31536000, "year"] )
 		"DEF:load2x=${datafile}:load2:AVERAGE",
 		"DEF:load3x=${datafile}:load3:AVERAGE",
 		"DEF:procs=${datafile}:procs:AVERAGE",
+		"DEF:procmin=${datafile}:procs:MIN",
+		"DEF:procmax=${datafile}:procs:MAX",
 		'CDEF:load1=load1x,100,*',
 		'CDEF:load2=load2x,100,*',
 		'CDEF:load3=load3x,100,*',
+		'CDEF:procrange=procmax,procmin,-',
+		'AREA:procmin',
+		'STACK:procrange#E0E0E0',
 		'AREA:load3#000099:loadavg3 [*100]',
 		'LINE2:load2#0000FF:loadavg2 [*100]',
 		'LINE1:load1#9999FF:loadavg1 [*100]',
-		'LINE1:procs#000000:processes'
+		'LINE1:procs#000000:processes',
 		);
     $ERR=RRDs::error;
     die "ERROR while drawing $datafile $time: $ERR\n" if $ERR;
