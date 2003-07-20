879e22fdd40b58e7044f23fa097651984ac8d003
mitch <mitch>
2003-07-20 09:22:51 +0000

    Grafik eingebaut

--- io.pl
+++ io.pl
@@ -1,5 +1,5 @@
 #!/usr/bin/perl
-# $Id: io.pl,v 1.1 2003-07-20 09:01:24 mitch Exp $
+# $Id: io.pl,v 1.2 2003-07-20 09:22:51 mitch Exp $
 #
 # RRD script to display io stats
 # 2003 (c) by Christian Garbs <mitch@cgarbs.de>
@@ -17,8 +17,8 @@ my $picbase  = "/home/mitch/pub/rrd/io-";
 
 # watch these paths
 my @dev = (
+	   "3,0",
 	   "3,1",
-	   "3,2",
 	   "",
 	   "",
 	   "",
@@ -40,22 +40,22 @@ chomp $hostname;
 if ( ! -e $datafile ) {
     # max 100% for each value
     RRDs::create($datafile,
-		 "DS:io0_read:GAUGE:600:0:U",
-		 "DS:io0_write:GAUGE:600:0:U",
-		 "DS:io1_read:GAUGE:600:0:U",
-		 "DS:io1_write:GAUGE:600:0:U",
-		 "DS:io2_read:GAUGE:600:0:U",
-		 "DS:io2_write:GAUGE:600:0:U",
-		 "DS:io3_read:GAUGE:600:0:U",
-		 "DS:io3_write:GAUGE:600:0:U",
-		 "DS:io4_read:GAUGE:600:0:U",
-		 "DS:io4_write:GAUGE:600:0:U",
-		 "DS:io5_read:GAUGE:600:0:U",
-		 "DS:io5_write:GAUGE:600:0:U",
-		 "DS:io6_read:GAUGE:600:0:U",
-		 "DS:io6_write:GAUGE:600:0:U",
-		 "DS:io7_read:GAUGE:600:0:U",
-		 "DS:io7_write:GAUGE:600:0:U",
+		 "DS:io0_read:COUNTER:600:0:U",
+		 "DS:io0_write:COUNTER:600:0:U",
+		 "DS:io1_read:COUNTER:600:0:U",
+		 "DS:io1_write:COUNTER:600:0:U",
+		 "DS:io2_read:COUNTER:600:0:U",
+		 "DS:io2_write:COUNTER:600:0:U",
+		 "DS:io3_read:COUNTER:600:0:U",
+		 "DS:io3_write:COUNTER:600:0:U",
+		 "DS:io4_read:COUNTER:600:0:U",
+		 "DS:io4_write:COUNTER:600:0:U",
+		 "DS:io5_read:COUNTER:600:0:U",
+		 "DS:io5_write:COUNTER:600:0:U",
+		 "DS:io6_read:COUNTER:600:0:U",
+		 "DS:io6_write:COUNTER:600:0:U",
+		 "DS:io7_read:COUNTER:600:0:U",
+		 "DS:io7_write:COUNTER:600:0:U",
 		 "RRA:AVERAGE:0.5:1:600",
 		 "RRA:AVERAGE:0.5:6:700",
 		 "RRA:AVERAGE:0.5:24:775",
@@ -105,27 +105,23 @@ RRDs::update($datafile,
 $ERR=RRDs::error;
 die "ERROR while updating $datafile: $ERR\n" if $ERR;
 
-exit 0;
-
-__DATA__
-
 # draw which values?
-my (@def, @line, @gprint);
+my (@def, @line);
 my $draw = 0;
 my $PI = 3.14159265356237;
-for my $idx ( 0..19 ) {
-    if ( $path[$idx] ne "" ) {
+$devs -- if $devs > 1;
+for my $idx ( 0..7 ) {
+    if ( $dev[$idx] ne "" ) {
 	my $color = sprintf '%02X%02X%02X'
-	    ,128 + (127 * sin ( 1 + $PI * ( $draw/$paths ) ) )
-	    ,128 + (127 * sin (     $PI * ( $draw/$paths ) ) )
-	    ,128 - (127 * sin ( 2 + $PI * ( $draw/$paths ) ) );
-	push @def, sprintf 'DEF:disk%02d=%s:disk%02d:AVERAGE', $idx, $datafile, $idx;
-	push @line, sprintf 'LINE2:disk%02d#%s:%s', $idx, $color, $path[$idx];
+	    ,128 + (127 * sin ( 1 + $PI * ( $draw/$devs ) ) )
+	    ,128 + (127 * sin (     $PI * ( $draw/$devs ) ) )
+	    ,128 - (127 * sin ( 2 + $PI * ( $draw/$devs ) ) );
+	push @def, sprintf 'DEF:io%d_read=%s:io%d_read:AVERAGE', $idx, $datafile, $idx;
+	push @def, sprintf 'DEF:io%d_writ=%s:io%d_write:AVERAGE', $idx, $datafile, $idx;
+	push @def, sprintf 'CDEF:io%d_write=0,io%d_writ,-', $idx, $idx;
+	push @line, sprintf 'LINE1:io%d_read#%s:(%s) in', $idx, $color, $dev[$idx];
+	push @line, sprintf 'LINE1:io%d_write#%s:(%s) out', $idx, $color, $dev[$idx];
 	$draw ++;
-	push @gprint, sprintf 'GPRINT:disk%02d:AVERAGE:%%3.0lf', $idx;
-	push @gprint, sprintf 'GPRINT:disk%02d:MIN:%%3.0lf', $idx;
-	push @gprint, sprintf 'GPRINT:disk%02d:MAX:%%3.0lf', $idx;
-	push @gprint, sprintf 'COMMENT:%s\n', $path[$idx];
     }
 }
 
@@ -136,17 +132,11 @@ foreach ( [3600, "hour"], [86400, "day"], [604800, "week"], [31536000, "year"] )
 		"--start=-$time",
 		'--lazy',
 		'--imgformat=PNG',
-		"--title=${hostname} disk usage (last $scale)",
-		'--upper-limit=100',
+		"--title=${hostname} io statistics (last $scale)",
 
 		@def,
 
-		@line,
-
-		'COMMENT:\n',
-		'COMMENT:\n',
-		'COMMENT:AVG  MIN  MAX  mount\n',
-		@gprint
+		@line
 		);
     $ERR=RRDs::error;
     die "ERROR while drawing $datafile $time: $ERR\n" if $ERR;
