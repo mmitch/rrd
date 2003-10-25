2e109482aeeb5da321757e954f961940a41ab849
mitch <mitch>
2003-10-25 22:17:34 +0000

    add /dev/hda

--- temperature.pl
+++ temperature.pl
@@ -1,5 +1,5 @@
 #!/usr/bin/perl
-# $Id: temperature.pl,v 1.10 2003-07-21 21:42:10 mitch Exp $
+# $Id: temperature.pl,v 1.11 2003-10-25 22:17:34 mitch Exp $
 #
 # RRD script to display hardware temperature
 # 2003 (c) by Christian Garbs <mitch@cgarbs.de>
@@ -88,6 +88,7 @@ foreach ( [3600, "hour"], [86400, "day"], [604800, "week"], [31536000, "year"] )
 		"DEF:fan0x=${datafile}:fan0:AVERAGE",
 		"DEF:temp0=${datafile}:temp0:AVERAGE",
 		"DEF:temp1=${datafile}:temp1:AVERAGE",
+		"DEF:disk00=${datafile}:disk00:AVERAGE",
 		"DEF:disk01=${datafile}:disk01:AVERAGE",
 		"DEF:disk02=${datafile}:disk02:AVERAGE",
 		"DEF:disk03=${datafile}:disk03:AVERAGE",
@@ -110,6 +111,7 @@ foreach ( [3600, "hour"], [86400, "day"], [604800, "week"], [31536000, "year"] )
 		'LINE1:temp0#000000',
 		'LINE2:fan0#8080FF:cpu fan [100r/m]',
 		'COMMENT:\n',
+		'LINE3:disk00#0000FF:hda [°C]',
 		'LINE3:disk01#FFFF00:hdb [°C]',
 		'LINE3:disk02#FF0000:hdc [°C] ',
 		'LINE2:disk03#00FF00:hdd [°C]',
