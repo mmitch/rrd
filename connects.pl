963884a1016d9de4861ad33749aa9c6b6755fde0
mitch <mitch>
2004-11-27 22:01:24 +0000

    initial version

---
+++ connects.pl
@@ -0,0 +1,68 @@
+#!/usr/bin/perl
+# $Id: connects.pl,v 1.1 2004-11-27 22:01:24 mitch Exp $
+#
+# RRD script to display io stats
+# 2003 (c) by Christian Garbs <mitch@cgarbs.de>
+# Licensed under GNU GPL.
+#
+# This script should be run every 5 minutes.
+#
+use strict;
+use warnings;
+use RRDs;
+
+# parse configuration file
+my %conf;
+eval(`cat ~/.rrd-conf.pl`);
+
+# set variables
+my $datafile = "$conf{DBPATH}/connects.rrd";
+my $picbase  = "$conf{OUTPATH}/connects-";
+
+# global error variable
+my $ERR;
+
+# whoami?
+my $hostname = `/bin/hostname`;
+chomp $hostname;
+
+# generate database if absent
+if ( ! -e $datafile ) {
+    # max 100% for each value
+    RRDs::create($datafile,
+		 "DS:connect:COUNTER:600:0:1000",
+		 "DS:disconnect:COUNTER:600:0:1000",
+		 "RRA:AVERAGE:0.5:1:600",
+		 "RRA:AVERAGE:0.5:6:700",
+		 "RRA:AVERAGE:0.5:24:775",
+		 "RRA:AVERAGE:0.5:288:797"
+		 );
+
+      $ERR=RRDs::error;
+      die "ERROR while creating $datafile: $ERR\n" if $ERR;
+      print "created $datafile\n";
+}
+
+# draw pictures
+foreach ( [3600, "hour"], [86400, "day"], [604800, "week"], [31536000, "year"] ) {
+    my ($time, $scale) = @{$_};
+    RRDs::graph($picbase . $scale . ".png",
+                "--start=-${time}",
+                '--lazy',
+                '--imgformat=PNG',
+                "--title=${hostname} TCP connections (last $scale)",
+                '--base=1024',
+		"--width=$conf{GRAPH_WIDTH}",
+		"--height=$conf{GRAPH_HEIGHT}",
+
+                "DEF:connect=${datafile}:active:AVERAGE",
+                "DEF:disconnect=${datafile}:passive:AVERAGE",
+
+		"CDEF:disconnect_down=0,disconnect,-",
+
+                'LINE1:disconnect_down#D00000:disconnects',
+                'LINE1:connect#00D000:connects',
+                );
+    $ERR=RRDs::error;
+    die "ERROR while drawing $datafile $time: $ERR\n" if $ERR;
+}
