92071b255662c72e521d6400e7163f1541141679
mitch <mitch>
2007-05-31 20:50:02 +0000

    use rrdupdate

--- bogofilter-procmailrc.sh
+++ bogofilter-procmailrc.sh
@@ -4,13 +4,13 @@ LINE=$(grep ^X-Bogosity: | head -n 1)
 [ "$LINE" ] || exit
 case ${LINE:12} in
 	Ham*)
-		rrdtool update $DATAFILE N:1:0:0
+		rrdupdate $DATAFILE N:1:0:0
 		;;
 	Unsure*)
-		rrdtool update $DATAFILE N:0:1:0
+		rrdupdate $DATAFILE N:0:1:0
 		;;
 	Spam*)
-		rrdtool update $DATAFILE N:0:0:1
+		rrdupdate $DATAFILE N:0:0:1
 		;;
 esac
 sleep 1
