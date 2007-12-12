1b86ae287b98967c5c1676458ad24864b98ccd26
mitch <mitch>
2007-12-12 17:43:28 +0000

    change upper limit

--- diskfree.pl
+++ diskfree.pl
@@ -1,5 +1,5 @@
 #!/usr/bin/perl
-# $Id: diskfree.pl,v 1.19 2007-08-05 13:31:35 mitch Exp $
+# $Id: diskfree.pl,v 1.20 2007-12-12 17:43:28 mitch Exp $
 #
 # RRD script to display disk usage
 # 2003,2007 (c) by Christian Garbs <mitch@cgarbs.de>
@@ -143,7 +143,7 @@ foreach ( [3600, 'hour'], [86400, 'day'], [604800, 'week'], [31536000, 'year'] )
 		"--width=$conf{GRAPH_WIDTH}",
 		"--height=$conf{GRAPH_HEIGHT}",
                 '--lower-limit=0',
-                '--upper-limit=100',
+                '--upper-limit=103',
 
 		@def,
 
