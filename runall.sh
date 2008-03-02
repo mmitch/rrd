68daddb8dbf58a47d83aedf4e67ca4136136dfd6
Christian Garbs <mitch@cgarbs.de>
2008-03-02 12:53:03 +0100

    pause value can be set from outside for easier testing

--- runall.sh
+++ runall.sh
@@ -1,40 +1,41 @@
 #!/bin/sh
-WAIT=17
+RRD_WAIT=${RRD_WAIT:-17}
+LANG=C
 
-/bin/sleep $WAIT
+/bin/sleep $RRD_WAIT
 /home/mitch/rrd/network.pl
-/bin/sleep $WAIT
+/bin/sleep $RRD_WAIT
 /home/mitch/rrd/tunnels.pl
-/bin/sleep $WAIT
+/bin/sleep $RRD_WAIT
 /home/mitch/rrd/temperature.pl 2> /dev/null
 
-/bin/sleep $WAIT
+/bin/sleep $RRD_WAIT
 /home/mitch/rrd/memory.pl
-/bin/sleep $WAIT
+/bin/sleep $RRD_WAIT
 /home/mitch/rrd/load.pl
-/bin/sleep $WAIT
+/bin/sleep $RRD_WAIT
 /home/mitch/rrd/diskfree.pl 2> /dev/null
 
-/bin/sleep $WAIT
+/bin/sleep $RRD_WAIT
 /home/mitch/rrd/ups.pl
-/bin/sleep $WAIT
+/bin/sleep $RRD_WAIT
 /home/mitch/rrd/cpu.pl
-/bin/sleep $WAIT
+/bin/sleep $RRD_WAIT
 
 /home/mitch/rrd/io.pl 2> /dev/null
-/bin/sleep $WAIT
-/home/mitch/rrd/netstat.pl
-/bin/sleep $WAIT
+/bin/sleep $RRD_WAIT
+/home/mitch/rrd/netstat.pl 2>&1 | fgrep -v 'error parsing /proc/net/snmp: Success'
+/bin/sleep $RRD_WAIT
 /home/mitch/rrd/dnscache.pl
-/bin/sleep $WAIT
+/bin/sleep $RRD_WAIT
 /home/mitch/rrd/firewall.pl
 
-/bin/sleep $WAIT
+/bin/sleep $RRD_WAIT
 /home/mitch/rrd/connecttime.pl
-/bin/sleep $WAIT
+/bin/sleep $RRD_WAIT
 /home/mitch/rrd/bogofilter.pl
-/bin/sleep $WAIT
+/bin/sleep $RRD_WAIT
 /home/mitch/rrd/cpufreq.pl
 
-/bin/sleep $WAIT
+/bin/sleep $RRD_WAIT
 /home/mitch/rrd/roundtrip.pl
