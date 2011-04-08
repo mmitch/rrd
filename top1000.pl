2e9d8673898b953d8b431d3dc061dff7e3192598
Christian Garbs <mitch@cgarbs.de>
2011-04-08 21:18:35 +0200

    add top1000.org
    drawing does not work yet

---
+++ top1000.pl
@@ -0,0 +1,110 @@
+#!/usr/bin/perl -w
+#
+# RRD script to display top1000.org usenet s
+# 2011 Copyright (C)  Christian Garbs <mitch@cgarbs.de>
+# Licensed under GNU GPL v3 or later.
+#
+# This script should be once a day..
+#
+
+use strict;
+use warnings;
+use RRDs;
+use LWP::Simple;
+
+# parse configuration file
+my %conf;
+eval(`cat ~/.rrd-conf.pl`);
+
+# set variables
+my $datafile = "$conf{DBPATH}/top1000.rrd";
+my $picbase  = "$conf{OUTPATH}/top1000-";
+
+my $mysite = $conf{TOP1000_ME};
+my @sites = @{$conf{TOP1000_SITES}};
+if (@sites < 10) {
+    $sites[9] = undef;
+}
+
+my $url_participants = 'http://www.top1000.org/participants.current.txt';
+my $url_top1000 =      'http://www.top1000.org/top1000.current.txt';
+
+# global error variable
+my $ERR;
+
+# whoami?
+my $hostname = `/bin/hostname`;
+chomp $hostname;
+
+# generate database if absent
+if ( ! -e $datafile ) {
+    RRDs::create($datafile,
+		 "--step=86400",
+		 "DS:submitted:ABSOLUTE:150000:0:U",
+		 "DS:site0:ABSOLUTE:150000:0:1000",
+		 "DS:site1:ABSOLUTE:150000:0:1000",
+		 "DS:site2:ABSOLUTE:150000:0:1000",
+		 "DS:site3:ABSOLUTE:150000:0:1000",
+		 "DS:site4:ABSOLUTE:150000:0:1000",
+		 "DS:site5:ABSOLUTE:150000:0:1000",
+		 "DS:site6:ABSOLUTE:150000:0:1000",
+		 "DS:site7:ABSOLUTE:150000:0:1000",
+		 "DS:site8:ABSOLUTE:150000:0:1000",
+		 "DS:site9:ABSOLUTE:150000:0:1000",
+		 'RRA:AVERAGE:0.5:1:370',
+		 'RRA:AVERAGE:0.5:7:370',
+		 'RRA:MIN:0.5:288:797'
+		 );
+      $ERR=RRDs::error;
+      die "ERROR while creating $datafile: $ERR\n" if $ERR;
+      print "created $datafile\n";
+}
+
+# get data
+my $content = get($url_participants) or die "Couldn't get $url_participants!";
+my $submitted = 0;
+foreach my $line (split /\n/, $content) {
+    if ($line =~ /^(\d+)\s+(\S+)/) {
+	if ($2 eq $mysite) {
+	    $submitted = $1;
+	}
+    }
+}
+
+$content = get($url_top1000) or die "Couldn't get $url_top1000!";
+my %site;
+foreach my $line (split /\n/, $content) {
+    if ($line =~ /^\s+(\d+)\s+(\d+\.\d+)\s+(\S+)/) {
+	my ($pos, $weight, $site) = ($1, $2, $3);
+	$site{$site} = $pos;
+    }
+}
+
+# update database
+RRDs::update($datafile,
+	     "N:$submitted:" . join (':', map { (defined $_ and defined $site{$_}) ? $site{$_} : 'U' } @sites )
+	     );
+$ERR=RRDs::error;
+die "ERROR while updating $datafile: $ERR\n" if $ERR;
+
+# draw pictures
+foreach ( [604800, "week"], [31536000, "year"] ) {
+    my ($time, $scale) = @{$_};
+    RRDs::graph($picbase . $scale . ".png",
+		"--start=-${time}",
+		'--lazy',
+		'--imgformat=PNG',
+		"--title=${hostname} usenet top1000 stats (last $scale)",
+		"--width=$conf{GRAPH_WIDTH}",
+		"--height=$conf{GRAPH_HEIGHT}",
+		
+		"DEF:submit_avg=${datafile}:submitted:AVERAGE",
+		"DEF:submit_min=${datafile}:submitted:MIN",
+
+		'LINE2:submit_min#FFD0D0:min submit',
+		'LINE1:submit_avg#D0FFD0:avg submit',
+		'COMMENT:\n',
+		);
+    $ERR=RRDs::error;
+    die "ERROR while drawing $datafile $time: $ERR\n" if $ERR;
+}
