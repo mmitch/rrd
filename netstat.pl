68d3cbc6796ab42b13855c1c24c48a81925b3673
Christian Garbs <mitch@cgarbs.de>
2019-10-17 21:45:29 +0200

    update netstat script [INCOMPATIBLE]
    - fix type of established connections from COUNTER to GAUGE
    - add additional metrics
    - these are incompatible changes!
      - delete your netstat.rrd database
      - or build a way to migrate your old data (I didn't)

--- netstat.pl
+++ netstat.pl
@@ -76,11 +76,17 @@ chomp $hostname;
 if ( ! -e $datafile ) {
     # max 100% for each value
     RRDs::create($datafile,
-		 "DS:active:COUNTER:600:0:50000",
-		 "DS:passive:COUNTER:600:0:50000",
-		 "DS:failed:COUNTER:600:0:50000",
-		 "DS:resets:COUNTER:600:0:50000",
-		 "DS:established:COUNTER:600:0:50000",
+		 "DS:active_open:COUNTER:600:0:5000000",
+		 "DS:passive_open:COUNTER:600:0:5000000",
+		 "DS:failed_conn:COUNTER:600:0:5000000",
+		 "DS:reset_in:COUNTER:600:0:5000000",
+		 "DS:reset_out:COUNTER:600:0:5000000",
+		 "DS:error_in:COUNTER:600:0:5000000",
+		 "DS:bad_checksum_in:COUNTER:600:0:5000000",
+		 "DS:segment_in:COUNTER:600:0:5000000",
+		 "DS:segment_out:COUNTER:600:0:5000000",
+		 "DS:segment_retransmit:COUNTER:600:0:5000000",
+		 "DS:established:GAUGE:600:0:500000",
 		 "RRA:AVERAGE:0.5:1:600",
 		 "RRA:AVERAGE:0.5:6:700",
 		 "RRA:AVERAGE:0.5:24:775",
@@ -105,7 +111,19 @@ my $values = proc_to_map($line, $nextline);
 
 # update database
 RRDs::update($datafile,
-	     join(':', 'N', $values->{ActiveOpens}, $values->{PassiveOpens}, $values->{AttemptFails}, $values->{EstabResets}, $values->{CurrEstab})
+	     join(':',
+		  'N',
+		  $values->{ActiveOpens},
+		  $values->{PassiveOpens},
+		  $values->{AttemptFails},
+		  $values->{EstabResets},
+		  $values->{OutRsts},
+		  $values->{InErrs},
+		  $values->{InCsumErrors},
+		  $values->{InSegs},
+		  $values->{OutSegs},
+		  $values->{RetransSegs},
+		  $values->{CurrEstab})
 	     );
 $ERR=RRDs::error;
 die "ERROR while updating $datafile: $ERR\n" if $ERR;
@@ -129,18 +147,36 @@ foreach ( [3600, "hour"], [86400, "day"], [604800, "week"], [31536000, "year"] )
 		'--logarithmic',
 		'--units=si',
 
-                "DEF:active=${datafile}:active:AVERAGE",
-                "DEF:passive=${datafile}:passive:AVERAGE",
-                "DEF:failed=${datafile}:failed:AVERAGE",
-                "DEF:resets=${datafile}:resets:AVERAGE",
+                "DEF:active_open=${datafile}:active_open:AVERAGE",
+                "DEF:passive_open=${datafile}:passive_open:AVERAGE",
+                "DEF:failed_conn=${datafile}:failed_conn:AVERAGE",
+                "DEF:reset_in=${datafile}:reset_in:AVERAGE",
+                "DEF:reset_out=${datafile}:reset_out:AVERAGE",
+                "DEF:error_in=${datafile}:error_in:AVERAGE",
+                "DEF:bad_checksum_in=${datafile}:bad_checksum_in:AVERAGE",
+#                "DEF:segment_in=${datafile}:segment_in:AVERAGE",
+#                "DEF:segment_out=${datafile}:segment_out:AVERAGE",
+                "DEF:segment_retransmit=${datafile}:segment_retransmit:AVERAGE",
                 "DEF:established=${datafile}:established:AVERAGE",
 
-                'LINE1:active#7000E0:active',
-                'LINE1:passive#60D050:passive',
-                'LINE1:failed#E0E000:failed',
-                'LINE1:resets#F0A000:resets',
-                'LINE1:established#E00070:established'
-                );
+                'LINE1:established#60D050:established',
+                'LINE1:active_open#C09000:open act ',
+                'LINE1:reset_in#8040D0:reset in ',
+                'LINE1:bad_checksum_in#E00070:bad checksum in:dashes=2',
+                'LINE1:error_in#E00070:error in',
+
+		'COMMENT:\n',
+		
+                'LINE1:failed_conn#FF9000:failed conn',
+                'LINE1:passive_open#E0C000:open pasv',
+                'LINE1:reset_out#4050D0:reset out',
+                'LINE1:segment_retransmit#808080:segment retransmit:dashes=2',
+#                'LINE1:segment_in#7000E0:sgmt in',
+#                'LINE1:segment_out#C000A0:sgmt out',
+
+		'COMMENT:\n',
+		
+	);
     $ERR=RRDs::error;
     die "ERROR while drawing $datafile $time: $ERR\n" if $ERR;
 }
